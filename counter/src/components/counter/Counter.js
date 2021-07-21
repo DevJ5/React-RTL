5ab61cb265b6d6a1029d3f3dd16e02141fb3c274
DevJ5 <idevelopjs@gmail.com>
2021-07-21 14:46:51 +0200

    BeforeEach

--- counter/src/components/counter/Counter.js
+++ counter/src/components/counter/Counter.js
@@ -3,27 +3,51 @@ import React, { useState } from 'react';
 const Counter = () => {
   const [count, setCount] = useState(0);
   const [inputValue, setInputValue] = useState(1);
+
+  const onAddClick = () => {
+    setCount(count + inputValue);
+  };
+
+  const onSubtractClick = () => {
+    setCount(count - inputValue);
+  };
+
+  const countClass = () => {
+    if (count >= 100) return 'counter__count--green';
+    if (count <= -100) return 'counter__count--red';
+  };
+
   return (
     <div className="counter">
       <h1 data-testid="header" className="counter__header">
         My counter
       </h1>
-      <p className="counter__count" data-testid="counter__count">
+      <p
+        className={`counter__count ${countClass()}`}
+        data-testid="counter__count"
+        aria-label="count">
         {count}
       </p>
-      <button className="counter__add-btn" data-testid="counter__add-btn">
-        +
+      <button
+        className="counter__subtract-btn"
+        data-testid="counter__subtract-btn"
+        onClick={onSubtractClick}>
+        -
       </button>
       <input
         type="number"
         className="counter__input"
         data-testid="counter__input"
         value={inputValue}
+        onChange={(e) => {
+          setInputValue(parseInt(e.target.value) || e.target.value);
+        }}
       />
       <button
-        className="counter__subtract-btn"
-        data-testid="counter__subtract-btn">
-        -
+        className="counter__add-btn"
+        data-testid="counter__add-btn"
+        onClick={onAddClick}>
+        +
       </button>
     </div>
   );
--- counter/src/components/counter/__test__/Counter.test.js
+++ counter/src/components/counter/__test__/Counter.test.js
@@ -1,37 +1,96 @@
-import { render, screen } from '@testing-library/react';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import Counter from '../Counter';
 
+let counterComponent;
+beforeEach(() => {
+  counterComponent = render(<Counter />);
+});
+
+afterEach(() => {
+  cleanup();
+});
+
+beforeAll(() => {});
+
+afterAll(() => {});
+
 test('header renders with correct text', () => {
-  const counterComponent = render(<Counter />);
-  const headerEl = counterComponent.getByTestId('header');
+  // const headerEl = counterComponent.getByTestId('header');
+  const headerEl = counterComponent.getByRole('heading', { level: 1 });
   expect(headerEl.textContent).toBe('My counter');
 });
 
 test('counter initially starts at 0', () => {
-  const counterComponent = render(<Counter />);
-  const countEl = counterComponent.getByTestId('counter__count');
+  // const countEl = counterComponent.getByTestId('counter__count');
+  const countEl = counterComponent.getByLabelText('count');
   expect(countEl.textContent).toBe('0');
 });
 
 test('input contains initial value of 1', () => {
-  const counterComponent = render(<Counter />);
-  const inputEl = counterComponent.getByTestId('counter__input');
+  // const inputEl = counterComponent.getByTestId('counter__input');
+  const inputEl = counterComponent.getByRole('spinbutton');
   expect(inputEl.value).toBe('1');
 });
 
 test('add button renders with +', () => {
-  const counterComponent = render(<Counter />);
-  const addBtnEl = counterComponent.getByTestId('counter__add-btn');
+  // const addBtnEl = counterComponent.getByTestId('counter__add-btn');
+  const addBtnEl = counterComponent.getByRole('button', { name: '+' });
   expect(addBtnEl.textContent).toBe('+');
 });
 
 test('subtract button renders with -', () => {
-  const counterComponent = render(<Counter />);
-  const subtractBtnEl = counterComponent.getByTestId('counter__subtract-btn');
+  // const subtractBtnEl = counterComponent.getByTestId('counter__subtract-btn');
+  const subtractBtnEl = counterComponent.getByRole('button', { name: /-/ });
   expect(subtractBtnEl.textContent).toBe('-');
 });
 
 test('change value of input works correctly', () => {
-  const counterComponent = render(<Counter />);
   const inputEl = counterComponent.getByTestId('counter__input');
+  userEvent.clear(inputEl);
+  userEvent.type(inputEl, '2');
+  // fireEvent.change(inputEl, {
+  //   target: { value: '2' },
+  // });
+  expect(inputEl.value).toBe('2');
+});
+
+test('pressing the + button increases the counter by the input value', () => {
+  const inputEl = counterComponent.getByTestId('counter__input');
+  const addBtnEl = counterComponent.getByRole('button', { name: '+' });
+  const countEl = counterComponent.getByLabelText('count');
+
+  userEvent.clear(inputEl);
+  userEvent.type(inputEl, '5');
+  userEvent.click(addBtnEl);
+  expect(countEl.textContent).toBe('5');
+});
+
+test('pressing the - button decreases the counter by the input value', () => {
+  const inputEl = counterComponent.getByTestId('counter__input');
+  const subtractBtnEl = counterComponent.getByRole('button', { name: /-/ });
+  const countEl = counterComponent.getByLabelText('count');
+
+  userEvent.clear(inputEl);
+  userEvent.type(inputEl, '5');
+  userEvent.click(subtractBtnEl);
+  expect(countEl.textContent).toBe('-5');
+});
+
+test('counter contains the correct className attribute', () => {
+  const inputEl = counterComponent.getByTestId('counter__input');
+  const addBtnEl = counterComponent.getByRole('button', { name: '+' });
+  const subtractBtnEl = counterComponent.getByRole('button', { name: /-/ });
+  const countEl = counterComponent.getByLabelText('count');
+
+  userEvent.clear(inputEl);
+  userEvent.type(inputEl, '100');
+  userEvent.click(addBtnEl);
+  expect(countEl.className).toBe('counter__count counter__count--green');
+
+  userEvent.click(subtractBtnEl);
+  expect(countEl.textContent).toBe('0');
+  userEvent.click(subtractBtnEl);
+  expect(countEl.className).toBe('counter__count counter__count--red');
 });
